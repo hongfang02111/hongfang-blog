b8dc2db449d6ea2fb0b942488b4a7d848b52b666
Hongfang Li <hongfang02111@gmail.com>
2022-08-08 21:10:04 -0400

    Updated meta info

--- app/root.tsx
+++ app/root.tsx
@@ -11,7 +11,7 @@ import styles from "./styles/app.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Xiaofeng Xie",
+  title: "Hongfang Li",
   viewport: "width=device-width,initial-scale=1",
 });
 
